820071e9202e000a8580191c4975b86b1c3fd890
Vish Mayer <vish.mayer@gmail.com>
2020-05-13 11:16:34 +0100

    print_dishes method added

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,4 +3,7 @@ require_relative 'basket'
 
 class Takeaway
 
+  def print_menu
+    "large pizza: £12.99"
+  end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,4 +2,11 @@ require 'takeaway'
 
 describe Takeaway do
 
+  subject(:takeaway) { described_class.new }
+
+  describe '#print_menu' do
+    it 'prints a list of dishes with their price' do
+      expect(takeaway.print_menu).to eq "large pizza: £12.99"
+    end
+  end
 end
